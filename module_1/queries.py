d1d22913af80748c9b38d77517da12fb4ef45442
dylanmhoke <dylan.m.hoke@outlook.com>
2023-09-22 15:28:44 -0700

    created several new queries

--- module_1/queries.py
+++ module_1/queries.py
@@ -1,4 +1,7 @@
-SELECT_ALL = "SELECT character_id,name FROM charactercreator_character;"
+SELECT_ALL = """
+    SELECT character_id,name
+    FROM charactercreator_character"""
+
 
 AVG_ITEM_WEIGHT_PER_CHARACTER = """
     SELECT ccc.name,avg(ai.weight) AS avg_item_weight
@@ -8,3 +11,75 @@ AVG_ITEM_WEIGHT_PER_CHARACTER = """
     JOIN armory_item as ai
     ON ai.item_id = ccci.item_id
     GROUP BY ccc.character_id"""
+
+
+TOTAL_CHARACTERS = """
+    SELECT count(character_id)
+    FROM charactercreator_character"""
+
+
+TOTAL_SUBCLASS = """
+    SELECT COUNT(character_id)
+    FROM charactercreator_character AS ccc
+    JOIN charactercreator_mage AS ccm 
+    ON ccc.character_id = ccm.character_ptr_id
+    JOIN charactercreator_necromancer AS ccn
+    ON ccm.character_ptr_id = ccn.mage_ptr_id"""
+
+
+TOTAL_ITEMS = """
+    SELECT item_id, count(*) as volume
+    FROM charactercreator_character_inventory
+    GROUP BY item_id"""
+
+
+WEAPONS = """
+    SELECT item_id,count(*)
+    FROM armory_item as ai
+    JOIN armory_weapon as aw
+    ON ai.item_id = aw.item_ptr_id
+    GROUP BY item_id"""
+
+
+NON_WEAPONS = """
+    SELECT item_id,count(*)
+    FROM armory_item
+    WHERE item_id
+    NOT IN (SELECT item_ptr_id FROM armory_weapon)"""
+
+
+CHARACTER_ITEMS = """
+    SELECT character_id,count(*)
+    FROM charactercreator_character_inventory
+    GROUP BY character_id
+    LIMIT 20"""
+
+
+CHARACTER_WEAPONS = """
+    SELECT character_id,COUNT(*)
+    FROM charactercreator_character_inventory AS ccci
+    JOIN armory_weapon AS aw
+    ON ccci.item_id = aw.item_ptr_id
+    GROUP BY character_id
+    LIMIT 20"""
+
+
+AVG_CHARACTER_ITEMS = """
+    SELECT AVG(item_count) as average_item_count
+    FROM (
+    SELECT character_id, count(*) AS item_count 
+    FROM charactercreator_character_inventory
+    GROUP BY character_id
+    ) AS subquery"""
+
+
+AVG_CHARACTER_WEAPONS = """
+    SELECT AVG(weapon_item_count) as average_item_count
+    FROM (
+    SELECT character_id,COUNT(*) AS weapon_item_count
+    FROM charactercreator_character_inventory AS ccci
+    JOIN armory_weapon AS aw
+    ON ccci.item_id = aw.item_ptr_id
+    GROUP BY character_id
+    ) AS subquery"""
+
