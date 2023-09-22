1088d1f3344d0babd0febc10515c19cd7bfa25aa
dylanmhoke <dylan.m.hoke@outlook.com>
2023-09-22 11:50:58 -0700

    Built a second query using DB Browser

--- module_1/queries.py
+++ module_1/queries.py
@@ -1,1 +1,8 @@
 SELECT_ALL = "SELECT character_id,name FROM charactercreator_character;"
+
+AVG_ITEM_WEIGHT_PER_CHARACTER = """SELECT ccc.name,avg(ai.weight) AS avg_item_weight FROM charactercreator_character as ccc
+JOIN charactercreator_character_inventory as ccci 
+ON ccc.character_id = ccci.character_id
+JOIN armory_item as ai
+ON ai.item_id = ccci.item_id
+GROUP BY ccc.character_id"""
--- module_1/sqlite_example.py
+++ module_1/sqlite_example.py
@@ -4,22 +4,36 @@ Beginning SQL
 import sqlite3
 import queries as q
 
-# Step 1
-# Connect to the database - check your spelling!
-connection = sqlite3.connect("rpg_db.sqlite3")
+#DB Connect function
+def connect_to_db(db_name="rpg_db.sqlite3"):
+    return sqlite3.connect(db_name)
 
 
-# Step 2
-# The teller will travel to the DB and pull information out
-cursor = connection.cursor()
-
-
-# Step 3 - write a query - (See the queries.py file)
-#change
-
-# Step 4 - Execute the query on the cursor and fetch the results
-#"pulling the results from the cursor"
-results = cursor.execute(q.SELECT_ALL).fetchall()
+def execute_q(conn,query):
+    #make the cursor 
+    curs = conn.cursor()
+    #execute the query
+    curs.execute(query)
+    #pull the results
+    return curs.fetchall()
 
 if __name__ == "__main__":
-    print(results[:5])
+    conn = connect_to_db()
+    print(execute_q(conn,q.SELECT_ALL)[:5])
+
+# # Step 1
+# # Connect to the database - check your spelling!
+# connection = sqlite3.connect("rpg_db.sqlite3")
+
+
+# # Step 2
+# # The teller will travel to the DB and pull information out
+# cursor = connection.cursor()
+
+
+# # Step 3 - write a query - (See the queries.py file)
+# #change
+
+# # Step 4 - Execute the query on the cursor and fetch the results
+# #"pulling the results from the cursor"
+# results = cursor.execute(q.SELECT_ALL).fetchall()
