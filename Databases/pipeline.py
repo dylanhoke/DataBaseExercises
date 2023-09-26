1b697561a7cbbfa8116024a2fd161b61ccbf0f49
dylanmhoke <dylan.m.hoke@outlook.com>
2023-09-26 11:38:35 -0700

    Added several queries and tools

---
+++ Databases/pipeline.py
@@ -0,0 +1,52 @@
+import psycopg2
+from sqlite_example import connect_to_db,execute_q
+from queries import GET_CHARACTERS,DROP_TEST_TABLE,CREATE_TEST_TABLE,INSERT_TEST_TABLE,INSERT_DYLAN,CREATE_CHARACTER_TABLE,DROP_CHARACTER_TABLE
+# from queries import INSERT_TEST_TABLE,CREATE_TEST_TABLE
+
+#postgreSQL connection credentials
+
+#user and default database
+DBNAME = "fkusbbvh"
+USER = "fkusbbvh"
+
+#password value
+PASSWORD = "IMhGNVeZgUlDnsOW63aXb55VesXVSQNt" 
+
+#server from SQL
+HOST =  "bubble.db.elephantsql.com"
+
+def connect_to_pg(dbname = DBNAME,user = USER,password = PASSWORD,host = HOST):
+    pg_conn = psycopg2.connect(dbname = DBNAME,user = USER,password = PASSWORD,host = HOST)
+    pg_curs = pg_conn.cursor()
+    return pg_conn, pg_curs
+
+def modify_db(conn,curs,query):
+    curs.execute(query)
+    conn.commit()
+
+
+if __name__ == "__main__":
+    #Get data from SQLite
+    sl_conn = connect_to_db()
+    sl_characters = execute_q(sl_conn, GET_CHARACTERS)
+
+    #Create destination table within PostGreSQL DB
+    pg_conn,pg_curs = connect_to_pg()
+    # modify_db(pg_conn,pg_curs,DROP_CHARACTER_TABLE)
+    modify_db(pg_conn,pg_curs,CREATE_CHARACTER_TABLE)
+
+    # print(sl_characters[:3])
+
+    #Loop over characters and insert into PostGreSQL with correct values
+    for character in sl_characters:
+        modify_db(pg_conn,pg_curs,
+            f"""
+            INSERT INTO characters ("name","level","exp","hp","strength",
+            "intelligence","dexterity","wisdom")
+            VALUES ('{character[1]}', {character[2]}, {character[3]},
+            {character[4]}, {character[5]}, {character[6]},
+            {character[7]}, {character[8]})
+            """
+        )
+
+    
--- Databases/queries.py
+++ Databases/queries.py
@@ -98,3 +98,48 @@ AVG_REVIEWS = """
     avg(Shopping),avg(Picnic) 
     FROM review"""
 
+
+CREATE_TEST_TABLE = """
+    CREATE TABLE IF NOT EXISTS test_table
+    ("id" SERIAL NOT NULL PRIMARY KEY,
+     "name" VARCHAR(200) NOT NULL,
+     "age" INT NOT NULL,
+     "country_of_origin" VARCHAR(200) NOT NULL);
+"""
+
+INSERT_TEST_TABLE = """
+    INSERT INTO test_table ("name","age","country_of_origin")
+    VALUES ('Dylan Hoke',30,'U.S.A');
+"""
+
+DROP_TEST_TABLE = """
+    DROP TABLE IF EXISTS test_table
+"""
+
+GET_CHARACTERS = """
+    SELECT * FROM charactercreator_character;
+"""
+
+CREATE_CHARACTER_TABLE = """
+    CREATE TABLE IF NOT EXISTS characters
+    ("character_id" SERIAL NOT NULL PRIMARY KEY,
+     "name" VARCHAR(200) NOT NULL,
+     "level" INT NOT NULL,
+     "exp" INT NOT NULL,
+     "hp" INT NOT NULL,
+     "strength" INT NOT NULL,
+     "intelligence" INT NOT NULL,
+     "dexterity" INT NOT NULL,
+     "wisdom" INT NOT NULL
+    );
+"""
+
+INSERT_DYLAN = """
+    INSERT INTO characters ("name","level","exp","hp","strength",
+    "intelligence","dexterity","wisdom")
+    VALUES ('Dylan Hoke',50,100,1000,9000,4,-5,12)
+"""
+
+DROP_CHARACTER_TABLE = """
+    DROP TABLE IF EXISTS character_table
+"""
