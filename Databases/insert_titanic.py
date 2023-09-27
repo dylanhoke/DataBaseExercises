1cc4bdd2161207061dd77c2a4fcddcb4bb741f16
dylanmhoke <dylan.m.hoke@outlook.com>
2023-09-27 08:46:41 -0700

    fixed issues with the titanic.py file

--- Databases/insert_titanic.py
+++ Databases/insert_titanic.py
@@ -1,39 +1,23 @@
 import psycopg2
 from os import getenv
 import pandas as pd
-from queries import DROP_CHARACTER_TABLE
-from sqlite_example import connect_to_db,execute_q
+
 #postgreSQL connection credentials
 
-#user and default database
 DBNAME = getenv("DBNAME")
 USER = getenv("USER")
+PASSWORD = getenv("PASSWORD")
+HOST = getenv("HOST")
 
-#password value
-PASSWORD = getenv("PASSWORD") 
+pg_conn = psycopg2.connect(dbname = DBNAME, user = USER, password = PASSWORD, host = HOST)
+pg_curs = pg_conn.cursor()
 
-#server from SQL
-HOST =  getenv("HOST")
-
-def connect_to_pg(dbname = DBNAME,user = USER,password = PASSWORD,host = HOST):
-    pg_conn = psycopg2.connect(dbname = DBNAME,user = USER,password = PASSWORD,host = HOST)
-    pg_curs = pg_conn.cursor()
-    return pg_conn, pg_curs
-
-def execute_q(conn,query):
-    #make the cursor 
-    curs = conn.cursor()
-    #execute the query
-    curs.execute(query)
-    #pull the results
-    return curs.fetchall()
-
-def modify_db(conn,curs,query):
+def execute_query_pg(curs,conn,query):
     results = curs.execute(query)
     conn.commit()
     return results
 
-TITANIC_TABLE = """
+CREATE_TITANIC_TABLE = """
     CREATE TABLE IF NOT EXISTS titanic_table(
         passenger_id SERIAL PRIMARY KEY,
         Survived INT NOT NULL,
@@ -44,25 +28,28 @@ TITANIC_TABLE = """
         Siblings_Spouses_Aboard INT NOT NULL,
         Parents_Children_Aboard INT NOT NULL,
         Fare float NOT NULL
-    )
+    );
+"""
+
+DROP_TITANIC_TABLE = """
+    DROP TABLE IF EXISTS titanic_table;
 """
 
 df = pd.read_csv("titanic.csv")
+df["Name"] = df["Name"].str.replace("'",'')
 
 if __name__ == "__main__":
 
-    pg_conn,pg_curs = connect_to_pg()
-    
     #Create the table and its associated schema
-    modify_db(pg_curs, pg_conn, DROP_CHARACTER_TABLE)
-    modify_db(pg_curs, pg_conn, TITANIC_TABLE)
+    execute_query_pg(pg_curs, pg_conn, DROP_TITANIC_TABLE)
+    execute_query_pg(pg_curs, pg_conn, CREATE_TITANIC_TABLE)
 
     records = df.values.tolist()
 
     for record in records:
         insert_statement ="""
-            INSERT INTO titanic_table ("Survived","Pclass","Name","Sex","Age",
-            "Siblings_Spouses_Aboard","Parents_Children_Aboard","Fare")
-            VALUES = {};
+            INSERT INTO titanic_table (Survived,Pclass,Name,Sex,Age,
+            Siblings_Spouses_Aboard,Parents_Children_Aboard,Fare)
+            VALUES {};
             """.format(tuple(record))
-        modify_db(pg_curs, pg_conn, insert_statement)
+        execute_query_pg(pg_curs, pg_conn, insert_statement)
